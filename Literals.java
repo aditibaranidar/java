2861e53e14847ff8175adaa6a02a80843f708e47
Aditi B <aditi.baranidar@gmail.com>
2025-12-30 11:20:58 -0800

    Literals Changes

---
+++ Literals.java
@@ -0,0 +1,22 @@
+class Literals
+{
+    public static void main(String a[])
+    {
+        // Int Literals 
+        int num1 = 585;
+        System.out.println(num1);
+
+        // Double Literals
+        double num2 = 56;  // Integer value can be assigned to double variable, will be converted to float
+        System.out.println(num2);
+
+        // Boolean Literals
+        boolean status = true; // Boolean literals cannot convert from int or other types
+        System.out.println(status);
+
+        // Character Literals
+        char c = 'a'; // Use the command c++ to increment character value
+        System.out.println(c);
+
+    }
+}
