848fe32efd3b075a39d00ed5f4717d2e7f4d5953
Aditi B <aditi.baranidar@gmail.com>
2026-01-04 18:58:56 -0800

    Relational Operators Changes

---
+++ RelationalOperators.java
@@ -0,0 +1,26 @@
+public class RelationalOperators 
+{
+    public static void main(String [] args)
+    {
+        int x = 6;
+        int y = 5;
+
+        boolean result = x > y; // Greater than operator
+        System.out.println(result);
+
+        boolean result1 = x < y; // Less than operator
+        System.out.println(result1);
+
+        boolean result2 = x >= y; // Greater than or equal to operator
+        System.out.println(result2);
+
+        boolean result3 = x <= y; // Less than or equal to operator
+        System.out.println(result3);
+
+        boolean result4 = x != y; // Not equal to operator
+        System.out.println(result4);
+
+        boolean result5 = x == y; // Equal to operator
+        System.out.println(result5);
+    }
+}
